e4815664c041223dc1000005b0a242407da2973f
Yeshaya <andyshapiro613gmail.com@hMBPslYeshaya.lan>
2024-07-03 11:01:11 -0400

    new home look

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,35 +1,33 @@
 import { useEffect, useState } from 'react';
 import {
   Container,
-  Background,
-  SevenSegmentDisplay,
-  Divider,
   StarField,
   Button,
-  Pager
+  SevenSegmentDisplay,
+  Divider,
 } from 'retro-react';
 
 function Home() {
   const [count, setCount] = useState(0);
-  const [_left, setLeft] = useState(100);
+  const [guestCount, setGuestCount] = useState(1);
+  const [marqueePosition, setMarqueePosition] = useState(100);
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setLeft(prevLeft => {
-        if (prevLeft < -100) {
-          return 100;
-        }
-        return prevLeft - 1;
-      });
-    }, 20);
+    // Simulating a guest counter
+    setGuestCount(Math.floor(Math.random() * 1000) + 1);
 
-    return () => clearInterval(timer);
+    // Marquee effect
+    const marqueeInterval = setInterval(() => {
+      setMarqueePosition((prevPos) => (prevPos <= -100 ? 100 : prevPos - 1));
+    }, 50);
+
+    return () => clearInterval(marqueeInterval);
   }, []);
 
   return (
     <>
       <StarField
-        numStars={1000}
+        numStars={3000}
         size={2}
         speed={1}
         starColor="white"
@@ -39,80 +37,62 @@ function Home() {
           left: 0,
           width: '100%',
           height: '100%',
-
+          zIndex: 1,
         }}
       />
       <div className="retro-container">
-        <Container
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            flexGrow: 1,
-            width: '55vw',
-            alignItems: 'center',
-            justifyContent: 'center',
-            position: 'relative',
-            height: 'calc(100vh + 100px)',
-            marginTop: '80px',
-          }}
-        >
-          <Background
-            color="#000000"
-            backgroundImage="https://eol.jsc.nasa.gov/Collections/EarthArt/img/CloudsSaudiArabia/ISS047-E-57170-57184_preview.jpg"
-            backgroundPosition="center center"
-            backgroundRepeat="repeat"
-            backgroundSize="cover"
-          >
-            <div className="content-wrapper">
-              <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: '100%'
-              }}>
-                <div className="retro-text">
-                  <p>Welcome to the yeshaya channel.</p>
-                  <Divider color="rainbow" orientation="horizontal" />
-                  <p>This place is my mind's little time capsule.</p>
-                  <Divider color="rainbow" orientation="horizontal" />
-                  <p>Make yourself at home.</p>
-                  <Divider color="rainbow" orientation="horizontal" />
-                  <p>In the good ol' days.</p>
-                </div>
-                <Button className="retro-button neon-button" onClick={() => setCount(count + 69)}>
-                  <SevenSegmentDisplay
-                    color="white"
-                    segmentThickness="none"
-                    value={count}
-                  />
-                </Button>
-                <p className="retro-text blink">which numbers have 69 in them?</p>
-                <div style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                }}>
-                  <Pager
-                    color="greyscale-dark"
-                    messages={['Sometimes', 'the', 'future', 'helps', 'us', 'experience', 'the', 'past.']}
-                    onButtonPress={function noRefCheck(){}}
-                  />
-                  <img src="https://web.archive.org/web/20030401014539im_/http://a772.g.akamai.net/7/772/51/c582cf249eee8e/www.apple.com/home/images/2003/03/promoimac03192003.gif" width="170" height="125" alt="The new iMac. 17-inch, 1GHz, iLife $1799" className="retro-img" />
-                </div>
-              </div>
-              <img
-                src="https://web.archive.org/web/20050827040031im_/http://image.weather.com/web/common/banners/desktopsevere.gif"
-                alt="weather banner"
-                style={{
-                  position: 'relative',
-                  bottom: 0,
-                  left: '50%',
-                  transform: 'translateX(-50%)',
-                }}
-                className="retro-img"
-              />
+        <Container>
+          <header>
+            <h1 className="moving-wordart">Welcome to Yeshaya's 90s Website!</h1>
+          </header>
+          <div className="marquee-container">
+            <div className="marquee-text" style={{ left: `${marqueePosition}%` }}>
+              🚀 Surf the information superhighway with style! 🌐 Grab your Palm Pilot and let's go! 📟
             </div>
-          </Background>
+          </div>
+          <main>
+            <div className="content-wrapper">
+              <img src="https://web.archive.org/web/20090830005036/http://geocities.com/Area51/Corridor/5177/aniufotny.gif" alt="UFO" className="retro-img" />
+              <p className="retro-text">Welcome to the yeshaya channel.</p>
+              <Divider color="rainbow" orientation="horizontal" />
+              <p className="retro-text">This place is my mind's little time capsule.</p>
+              <Divider color="rainbow" orientation="horizontal" />
+              <p className="retro-text">Make yourself at home.</p>
+              <Divider color="rainbow" orientation="horizontal" />
+              <p className="retro-text">In the good ol' days.</p>
+              <Button className="retro-button neon-button" onClick={() => setCount(count + 69)}>
+                <SevenSegmentDisplay
+                  color="lime"
+                  segmentThickness="thick"
+                  value={count}
+                />
+              </Button>
+              <p className="retro-text blink">which numbers have 69 in them?</p>
+              <div className="guest-counter">
+                <p>You are visitor number: {guestCount}</p>
+              </div>
+            </div>
+            <div className="ad-section">
+              <div className="retro-ad" style={{ borderColor: 'cyan' }}>
+                <h3 style={{ color: 'cyan' }}>NEW! Pentium III</h3>
+                <img src="https://web.archive.org/web/20090829232453/http://geocities.com/Tokyo/Fuji/1296/pentium.gif" alt="Pentium III" />
+                <p>Blazing fast 500MHz processor!</p>
+              </div>
+              <div className="retro-ad" style={{ borderColor: 'magenta' }}>
+                <h3 style={{ color: 'magenta' }}>AOL CD-ROM</h3>
+                <img src="https://web.archive.org/web/20090830031745/http://geocities.com/Hollywood/Lot/6038/aol_gif.gif" alt="AOL CD-ROM" />
+                <p>1000 FREE hours!</p>
+              </div>
+            </div>
+          </main>
+          <footer>
+            <img
+              src="https://web.archive.org/web/20090829225554/http://geocities.com/TimesSquare/Arcade/3183/construction.gif"
+              alt="Under Construction"
+              className="retro-img"
+            />
+            <p className="retro-text">© 1999 Yeshaya's Rad Website. Best viewed in Netscape Navigator.</p>
+          </footer>
         </Container>
       </div>
     </>
